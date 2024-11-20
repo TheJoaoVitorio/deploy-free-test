ad9d1863980d0d878651891d9540086fd6d21c40
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-11-20 12:30:42 -0300

    testando rota com conexao da neon com postgres

--- src/server.ts
+++ src/server.ts
@@ -6,20 +6,37 @@ app.use(express.json())
 
 const port = process.env.PORT ?? 4000
 
-app.get('/books', async (request, response) => {
-  const books = await prismaClient.book.findMany()
-  return response.json(books)
-})
+app.get('/projetos/:id', async (req, res) => {
+  try {
+      const { id } = req.params;
+      const intId = parseInt(id);
 
-app.post('/books', async (request, response) => {
-  const { description, name } = request.body
-  const book = await prismaClient.book.create({
-    data: {
-      description,
-      name,
-    },
-  })
-  return response.json(book)
-})
+      if (isNaN(intId)) {
+          return res.status(400).json({ error: "ID deve ser um número válido" });
+      }
+
+      const projectExists = await prisma.Project.findUnique({
+          where: { id: intId }
+      });
+
+      if (!projectExists) {
+          return res.status(404).json({ error: "Projeto não encontrado" });
+      }
+
+      // Atualiza as views do projeto
+      const updateViewsProject = await prisma.Project.update({
+          where: { id: intId },
+          data: {
+              views: projectExists.views + 1
+          }
+      })
+
+      return res.status(200).json(projectExists);
+
+  } catch (error) {
+      return res.status(500).json({ error: "Erro interno do servidor" });
+  }
+});
+
 
 app.listen(port, () => console.log('Server is running on port ', port))
