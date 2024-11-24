1548f297b7413baa32ef7bc67be09ae9250717f6
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-11-24 11:28:07 -0300

    route add

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'cors'; // Importa a biblioteca de CORS
+import cors from 'cors';
 import { prismaClient } from './database';
 
 const app = express();
@@ -9,10 +9,11 @@ const port = process.env.PORT ?? 4000;
 
 // Configuração de CORS
 app.use(cors({
-    origin: 'https://joaovitoriodev.vercel.app', // Substitua pela origem permitida
+    origin: 'https://joaovitoriodev.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
 }));
 
+// GET Projetos
 app.get('/projetos/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -44,5 +45,38 @@ app.get('/projetos/:id', async (req, res) => {
         return res.status(500).json({ error: "Erro interno do servidor" });
     }
 });
+
+
+// POST Views no Projeto
+
+
+// POST Likes no Projeto
+app.post("/projetos/:id/like", async (req, res) => {
+    const { id } = req.params;
+    const intId = parseInt(id);
+
+    if (isNaN(intId)) {
+        return res.status(400).json({ error: "ID deve ser um número válido" });
+    }
+
+    const projectExists = await prismaClient.project.findUnique({
+        where: { id: intId }
+    });
+
+    if (!projectExists) {
+        return res.status(404).json({ error: "Projeto não encontrado" });
+    }
+
+    const updateLikesProject = await prismaClient.project.update({
+        where: {id:intId},
+        data: {
+            likes: projectExists.likes + 1
+        }
+    })
+
+    return res.status(200).json(projectExists);
+
+} )
+
 
 app.listen(port, () => console.log('Server is running on port ', port));
